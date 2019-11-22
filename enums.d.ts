b304bd433f186fe43fd35b352853d0e08dae45be
leo266 <57928651+leo266@users.noreply.github.com>
2019-11-22 18:01:44 +0800

    Update enums.d.ts

--- enums.d.ts
+++ enums.d.ts
@@ -25,29 +25,28 @@
 
 
     declare enum RemoteButton {   
-     // CH_MINUS = 0x45,
-	//  CH = 0x46,
-	//  CH_Add = 0x47,
-	//  PREV = 0x44,
-        //PLAY = 0x43,
-	//  NUM_200 = 0xd,
-	//  NEXT = 0x40,
-	//  NUM_100 = 0x19,
-	//  Minus = 0x7,
-	//  Add = 0x15,
-	//  EQ = 0x9,
-	//  NUM0 = 0x16,
-	//  NUM1 = 0xc,
-	//  NUM2 = 0x18,
-	//  NUM3 = 0x5e,
-	//  NUM4 = 0x8,
-	//  NUM5 = 0x1c,
-	//  NUM6 = 0x5a,
-	//  NUM7 = 0x42,
-	//  NUM8 = 0x52,
+          //CH_MINUS = 0x45,
+	  //CH = 0x46,
+	  //CH_Add = 0x47,
+	  //PREV = 0x44,
+          //PLAY = 0x43,
+	  //NUM_200 = 0xd,
+	  //NEXT = 0x40,
+	  //NUM_100 = 0x19,
+	  //Minus = 0x7,
+	  //Add = 0x15,
+	  //EQ = 0x9,
+	  //NUM0 = 0x16,
+	  //NUM1 = 0xc,
+	  //NUM2 = 0x18,
+	  //NUM3 = 0x5e,
+	  //NUM4 = 0x8,
+	  //NUM5 = 0x1c,
+	  //NUM6 = 0x5a,
+	  //NUM7 = 0x42,
+	  //NUM8 = 0x52,
           //NUM9 = 0x4A,
-	  
-	    //CH_MINUS = 0x45,
+	  //CH_MINUS = 0x45,
 	  UP = 0x46,
 	  //CH_Add = 0x47,
 	  LEFT = 0x44,
@@ -67,11 +66,7 @@
 	  NUM9 = 0x5a,
 	  STAR= 0x42,
 	  NUM0 = 0x52,
-	  GRID= 0x4A,
-	    
-	    
-	    
-	    
+	  GRID= 0x4A,    
     }
 declare namespace Mbit_IR {
 }
